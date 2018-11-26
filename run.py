31e069e1cb9c42e089d382f6b3e117bc7f16840e
haohe <hao.he@foxmail.com>
2018-11-26 11:42:48 +0800

    添加swagger插件

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+
 from eve import Eve
+from eve_swagger import swagger
 
 import hooks
 
@@ -14,11 +16,23 @@ else:
     host = '127.0.0.1'
 
 app = Eve()
+app.register_blueprint(swagger)
 app.on_pre_POST_devices += hooks.pre_devices_post_callback
 app.on_pre_PUT_devices += hooks.pre_devices_put_callback
 # app.on_post_POST_devices += hooks.post_devices_post_callback
 app.on_insert_devices += hooks.on_insert_devices
 app.on_replace_devices += hooks.on_replace_devices
+
+app.config['SWAGGER_INFO'] = {
+    'title': '视频集成HTTP API',
+    'version': '1.0',
+    'description': '通过接口添加DVR、NVR、IPC等设备,添加成功后返回通道的信息并且开始采集视频到SRS中',
+    'contact': {
+        'name': 'HaoHe',
+        'email': 'hao.he@foxmail.com'
+    },
+    'schemes': ['http', 'https'],
+}
 
 if __name__ == '__main__':
     app.run(host=host, port=port, debug='DEBUG' in os.environ)
